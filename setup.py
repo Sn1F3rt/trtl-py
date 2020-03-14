0444b97fbab59305fbefd3bfd32e42278075bcbd
Sayan Bhattacharyya <sohamb03@outlook.com>
2020-03-14 07:16:44 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'trtl-py',         
+  packages = ['trtl-py'],
+  version = '0.1',      
+  license='MIT',        
+  description = 'The Python RPC Wrapper for TurtleCoind and WalletAPI.',   
+  author = 'Sayan Bhattacharyya',                
+  author_email = 'sohamb03@outlook.com',    
+  url = 'https://github.com/sohamb03/trtl-py', 
+  download_url = 'https://github.com/sohamb03/trtl-py/archive/v_01.tar.gz',    
+  keywords = ['turtlecoin', 'turtlecoind', 'wallet-api', 'py-wrapper'],   
+  install_requires=[           
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
