789b42d295ee2897134270c28bc7955ccfecc0b9
Sayan Bhattacharyya <sohamb03@outlook.com>
2020-04-11 17:31:16 +0530

    Update TurtleCoind.py

--- trtl/TurtleCoind.py
+++ trtl/TurtleCoind.py
@@ -7,30 +7,37 @@ class TurtleCoind:
     Integrates with JSON-RPC interface of `TurtleCoind`
     """
 
-    def __init__(self, host = '127.0.0.1', port = 11898, ssl = False):
+    def __init__(self, host='127.0.0.1', port=11898, ssl=False):
         if ssl:
             self.url = f'https://{host}:{port}'
         else:
             self.url = f'http://{host}:{port}'
+
         self.headers = {'content-type': 'application/json'}
 
     def _get_request(self, method):
         get_url = self.url + '/' + method
 
         response = requests.get(get_url)
+
         return response.json()
 
-    def _post_request(self, method, **kwargs):
-        post_url = self.url +'/json_rpc'
+    def _post_request(self, method, params):
+        post_url = self.url + '/json_rpc'
+
         payload = {
             'jsonrpc': '2.0',
-            'method' : method,
-            'params': kwargs
+            'method': method,
+            'params': params
         }
 
-        response = requests.post(post_url, data=json.dumps(payload), headers=self.headers).json()
+        response = requests.post(post_url,
+                                 data=json.dumps(payload),
+                                 headers=self.headers).json()
+
         if 'error' in response:
             raise ValueError(response['error'])
+
         return response
 
     def get_block_count(self):
@@ -38,7 +45,7 @@ class TurtleCoind:
         Returns current chain height.
         """
 
-        return self._post_request('getblockcount')
+        return self._post_request('getblockcount', {})
 
     def get_block_hash(self, height):
         """
@@ -46,7 +53,8 @@ class TurtleCoind:
         """
 
         params = [height]
-        return self._post_request('on_getblockhash', **params)
+
+        return self._post_request('on_getblockhash', params)
 
     def get_block_template(self, reserve_size, wallet_address):
         """
@@ -55,7 +63,8 @@ class TurtleCoind:
 
         params = {'reserve_size': reserve_size,
                   'wallet_address': wallet_address}
-        return self._post_request('getblocktemplate', **params)
+
+        return self._post_request('getblocktemplate', params)
 
     def submit_block(self, block_blob):
         """
@@ -63,22 +72,24 @@ class TurtleCoind:
         """
 
         params = [block_blob]
-        return self._post_request('submitblock', **params)
+
+        return self._post_request('submitblock', params)
 
     def get_last_block_header(self):
         """
         Returns last block header.
         """
 
-        return self._post_request('getlastblockheader')
+        return self._post_request('getlastblockheader', {})
 
-    def get_block_header_by_hash(self, hash):
+    def get_block_header_by_hash(self, txn_hash):
         """
         Returns last block header by given hash.
         """
 
-        params = {'hash': hash}
-        return self._post_request('getblockheaderbyhash', **params)
+        params = {'hash': txn_hash}
+
+        return self._post_request('getblockheaderbyhash', params)
 
     def get_block_header_by_height(self, height):
         """
@@ -86,6 +97,7 @@ class TurtleCoind:
         """
 
         params = {'height': height}
+
         return self._post_request('getblockheaderbyheight', **params)
 
     def get_currency_id(self):
@@ -93,7 +105,7 @@ class TurtleCoind:
         Returns unique currency identifier.
         """
 
-        return self._post_request('getcurrencyid')
+        return self._post_request('getcurrencyid', {})
 
     def get_blocks(self, height):
         """
@@ -101,7 +113,8 @@ class TurtleCoind:
         """
 
         params = {'height': height}
-        return self._post_request('f_blocks_list_json', **params)
+
+        return self._post_request('f_blocks_list_json', params)
 
     def get_block(self, block_hash):
         """
@@ -109,19 +122,21 @@ class TurtleCoind:
         """
 
         params = {'hash': block_hash}
-        return self._post_request('f_block_json', **params)
+
+        return self._post_request('f_block_json', params)
 
     def get_transaction(self, transaction_hash):
         """
-        Gets information on the single transaction
+        Returns information on the single transaction
         """
 
-        params = {'hash' : transaction_hash}
-        return self._post_request('f_transaction_json', **params)
+        params = {'hash': transaction_hash}
+
+        return self._post_request('f_transaction_json', params)
 
     def get_transaction_pool(self):
         """
-        Gets the list of transaction hashs in the mempool.
+        Returns the list of transaction hashes in the mempool.
         """
 
-        return self._post_request('f_on_transactions_pool_json')
+        return self._post_request('f_on_transactions_pool_json', {})
