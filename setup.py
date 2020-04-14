3ab80ebe07c4f95e1c6e4ba808c3cfa808291081
Sayan Bhattacharyya <sohamb03@outlook.com>
2020-04-14 11:41:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
   url = 'https://github.com/sohamb03/trtl-py', 
   download_url = 'https://github.com/sohamb03/trtl-py/archive/v_01.tar.gz',    
   keywords = ['turtlecoin', 'turtlecoind', 'wallet-api', 'py-wrapper'],   
+  python_requires=">=3.6",
   install_requires=[           
           'requests'
       ],
@@ -23,4 +24,7 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
   ],
+  project_urls={
+        'Documentation': 'https://trtl-py.sohamb03.me',
+  },
 )
